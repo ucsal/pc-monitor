048677b8dde66f65fb9166994a98e1cc5f900143
dgs-small <lucas9667@hotmail.com>
2022-05-09 22:26:34 -0300

    adicionando diretório com aplicação web em spring boot

---
+++ pc-monitor-spring/src/main/java/br/ucsal/pcmonitorspring/PcMonitorSpringApplication.java
@@ -0,0 +1,13 @@
+package br.ucsal.pcmonitorspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcMonitorSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcMonitorSpringApplication.class, args);
+	}
+
+}
---
+++ pc-monitor-spring/src/test/java/br/ucsal/pcmonitorspring/PcMonitorSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ucsal.pcmonitorspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcMonitorSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
