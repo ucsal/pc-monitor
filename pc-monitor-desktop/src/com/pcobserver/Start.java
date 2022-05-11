0d49aa034a9a3e8370d56c3c2f82090531a5b655
LGBastos <66877447+LGBastos@users.noreply.github.com>
2022-05-11 17:22:09 -0300

    Adicionado o TrayIcon #1

--- pc-monitor-desktop/src/com/pcobserver/Start.java
+++ pc-monitor-desktop/src/com/pcobserver/Start.java
@@ -2,6 +2,7 @@ package com.pcobserver;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.net.URL;
 
 
 public class Start {
@@ -15,24 +16,27 @@ public class Start {
 	         // get the SystemTray instance
 	         SystemTray tray = SystemTray.getSystemTray();
 	         // load an image
-	         Image image = Toolkit.getDefaultToolkit().getImage(getClass().getClassLoader().getResource("computador.png"));
-	         ActionListener listener = new ActionListener() {
+			 final URL pngComputador = getClass().getResource("computador.png");
+	         Image image = Toolkit.getDefaultToolkit().createImage(pngComputador);
+	         ActionListener exitListener = new ActionListener() {
 	             public void actionPerformed(ActionEvent e) {
 	                 // execute default action of the application
-	                 // ...
+					 System.out.println("Encerrado");
+					 System.exit(0);
 	             }
 	         };
-	         // create a popup menu
+//	         // create a popup menu
 	         PopupMenu popup = new PopupMenu();
-	         // create menu item for the default action
-	         MenuItem defaultItem = new MenuItem();
-	         defaultItem.addActionListener(listener);
+//	         // create menu item for the default action
+	         MenuItem defaultItem = new MenuItem("Encerrar");
+	         defaultItem.addActionListener(exitListener);
 	         popup.add(defaultItem);
 	         /// ... add other items
 	         // construct a TrayIcon
 	         trayIcon = new TrayIcon(image, "PC Observer", popup);
+			 trayIcon.setImageAutoSize(true);
 	         // set the TrayIcon properties
-	         trayIcon.addActionListener(listener);
+//	         trayIcon.addActionListener(listener);
 	         // ...
 	         // add the tray image
 	         try {
