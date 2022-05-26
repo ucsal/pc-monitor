cf76207963a88367ddf3f440a1c63a7cc540d772
eduardodonadon <dona.edu2@gmail.com>
2022-05-26 20:30:35 -0300

    start get cpu usage

--- pc-monitor-desktop/src/com/pcobserver/utils/getData.java
+++ pc-monitor-desktop/src/com/pcobserver/utils/getData.java
@@ -1,15 +1,35 @@
 package com.pcobserver.utils;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class getData {
 	public static String getOsName() {
-        return System.getProperty("os.name");
-    }
-	
+		return System.getProperty("os.name");
+	}
+
 	public static String getJavaVersion() {
-        return System.getProperty("java.version");
-    }	
-	
+		return System.getProperty("java.version");
+	}	
+
 	public static String getUserName() {
-		 return System.getProperty("user.name");
+		return System.getProperty("user.name");
+	}
+
+	public static String getCpuUsage() throws IOException {
+		Process process;
+		
+
+		process = Runtime.getRuntime().exec("wmic cpu get loadpercentage");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+		
+		String line = "";
+	    while ((line = reader.readLine()) != null) {
+	        System.out.println(line);
+	        if(!line.contains("LoadPercentage")) return line;
+	    }
+
+		return null;
 	}
 }
