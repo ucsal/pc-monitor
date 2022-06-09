a93d7ecd7727bd16a1dc6e9974b83f35749bcbe3
dgs-small <lucas9667@hotmail.com>
2022-06-09 20:38:52 -0300

    Rota para login e validação inicial, service para validar usuários, modificação no repositório de WebUser, arquivo sql para inserção de user admin

---
+++ pc-monitor-spring/src/main/java/br/ucsal/pcmonitorspring/controller/Login.java
@@ -0,0 +1,29 @@
+package br.ucsal.pcmonitorspring.controller;
+
+import br.ucsal.pcmonitorspring.services.WebUserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class Login {
+
+    private WebUserService service = new WebUserService();
+
+    @GetMapping("/login")
+    public String login(Model model){
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String validate(Model model) {
+        if (service.isRegistered((String) model.getAttribute("login"))){
+            return "/dashboard/ambientes";
+        }
+        else {
+            model.addAttribute("erro", "Usuário Inválido");
+            return "/login";
+        }
+    }
+}
