edd4294e0786c30ff2a88b9c252a99749f2d8c71
eduardodonadon <dona.edu2@gmail.com>
2022-06-11 01:25:46 -0300

    mapping interceptor to dashboard routes

--- pc-monitor-spring/src/main/java/br/ucsal/pcmonitorspring/controller/Login.java
+++ pc-monitor-spring/src/main/java/br/ucsal/pcmonitorspring/controller/Login.java
@@ -1,11 +1,17 @@
 package br.ucsal.pcmonitorspring.controller;
 
+import br.ucsal.pcmonitorspring.model.WebUserModel;
 import br.ucsal.pcmonitorspring.services.WebUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class Login {
@@ -13,19 +19,25 @@ public class Login {
     @Autowired
     private WebUserService webUserService;
 
+    @Autowired
+    private HttpServletRequest httpServletRequest;
+
     @GetMapping("/login")
     public String login(Model model){
+        model.addAttribute("webUser", new WebUserModel());
         return "login";
     }
 
     @PostMapping("/login")
-    public String validate(Model model) {
-        
-        if (webUserService.isRegistered((String) model.getAttribute("login"))){
-            return "/dashboard/ambientes";
+    public String validate(@ModelAttribute("webUser") WebUserModel webUser, Model model) {
+        if (webUserService.isRegistered(webUser.getLogin())){
+            HttpSession session = httpServletRequest.getSession();
+            session.setAttribute("login", webUser.getLogin());
+
+            return "/home";
         }
         else {
-            model.addAttribute("erro", "Usuário Inválido");
+//            model.addAttribute("erro", "Usuário Inválido");
             return "/login";
         }
     }
--- pc-monitor-spring/src/main/java/br/ucsal/pcmonitorspring/interceptors/EnsureAuthentication.java
+++ pc-monitor-spring/src/main/java/br/ucsal/pcmonitorspring/interceptors/EnsureAuthentication.java
@@ -1,17 +1,24 @@
 package br.ucsal.pcmonitorspring.interceptors;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+@Component
 public class EnsureAuthentication implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         HttpSession session = request.getSession();
-        if (session.getAttribute("login") == null)
+        String se = (String) session.getAttribute("login");
+
+        if (session.getAttribute("login") == null){
+            System.out.println("BLOCK");
             return false;
+        }
+
         return HandlerInterceptor.super.preHandle(request, response, handler);
     }
 }
