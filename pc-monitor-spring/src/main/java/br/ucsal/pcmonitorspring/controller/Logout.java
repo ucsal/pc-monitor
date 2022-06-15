84e6baacddc1640ba55b565f77c954a77d8cf2f4
eduardodonadon <dona.edu2@gmail.com>
2022-06-14 21:41:33 -0300

    improve logout controller

--- pc-monitor-spring/src/main/java/br/ucsal/pcmonitorspring/controller/Logout.java
+++ pc-monitor-spring/src/main/java/br/ucsal/pcmonitorspring/controller/Logout.java
@@ -2,21 +2,20 @@ package br.ucsal.pcmonitorspring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class Logout {
 
     @Autowired
-    HttpServletRequest request;
+    HttpSession session;
 
     @GetMapping("/logout")
     public RedirectView logout(){
-        request.getSession().invalidate();
+        session.invalidate();
         return new RedirectView("/login");
     }
 }
